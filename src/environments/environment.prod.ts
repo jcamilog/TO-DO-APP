0770d2bd0ecbd7abc935abefe54e2e98b964a1a3
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-09-15 22:16:03 -0500

    add key firebase ad production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD7annOschK9i9bbiM-uFwVWuNUkOS0Srs',
+    authDomain: 'platzi-firebase-aa930.firebaseapp.com',
+    databaseURL: 'https://platzi-firebase-aa930.firebaseio.com',
+    projectId: 'platzi-firebase-aa930',
+    storageBucket: 'platzi-firebase-aa930.appspot.com',
+    messagingSenderId: '385667404775',
+    appId: '1:385667404775:web:a8966db6066c3bdffda342',
+    measurementId: 'G-XE56F2JCF4'
+  }
 };
